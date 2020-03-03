755dd333bb36339fb055749f00c81d17df155491
przekwas <luke@covalence.io>
2020-03-02 23:52:46 -0600

    testing build

---
+++ build/precache-manifest.b582baedb2d332235624aedab6746f1f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ac1792f5377e2b9124c0be142bb4c3c3",
+    "url": "/chirper-bbva-demo/index.html"
+  },
+  {
+    "revision": "3f6081bbbedf7201606f",
+    "url": "/chirper-bbva-demo/static/css/2.47e06e2e.chunk.css"
+  },
+  {
+    "revision": "3f6081bbbedf7201606f",
+    "url": "/chirper-bbva-demo/static/js/2.16813cea.chunk.js"
+  },
+  {
+    "revision": "eff3b4d7e4bb0087ce53139cd90bc880",
+    "url": "/chirper-bbva-demo/static/js/2.16813cea.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7b4af89f3b9f5fcdf645",
+    "url": "/chirper-bbva-demo/static/js/main.4c75fbdf.chunk.js"
+  },
+  {
+    "revision": "0d0d93b88f42470b24c2",
+    "url": "/chirper-bbva-demo/static/js/runtime-main.4e593488.js"
+  }
+]);
